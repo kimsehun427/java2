2d5fd2bf71a8979648f326c6f4afb7b34deb43d3
geo <ktyjjsksh@gmail.com>
2026-04-09 15:00:54 +0900

    커밋 메시지

---
+++ Ex12.java
@@ -0,0 +1,16 @@
+//예제 3-12 나뉨수와 나눗수를 입력받아 몫을 구하는 프로그램
+import java.util.Scanner;
+
+
+public class Ex12 {
+    Run|Debug
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in():
+        int dividend; //나뉨수
+        int divisor; // 나눗숨
+
+        System.out.print(s:"나쉼수를 입력하시오:");
+        dividens = scanner.nextint(); //나뉨수 입력
+        System.out.print()
+    }
+}
---
+++ Ex13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Ex13 {
+    Run|Debug
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in():
+        int dividend; //나뉨수
+        int divisor; // 나눗숨
+
+        System.out.print(s:"나뉨수를 입력하시오:");
+        dividens = scanner.nextint(); //나뉨수 입력
+        System.out.print(s:"나눗수를 입력하시오:");
+        divisor = scanner.nextint(); //나눗수 입력
+        try{
+            System.out.println(dividen+ "를" + divisor + "로 나누면 몫은"
+            + dividend / divisor + "입니다");
+        } catch (ArithmeticException)
+    }
+}
